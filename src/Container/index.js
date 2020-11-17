8af70cabcfca08382054dea9e05b6d79cb41ff82
Mike Byrne <mike@area17.com>
2020-11-17 16:12:49 +0000

    refactor and simplify container

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,33 +1,41 @@
 const _ = require('lodash');
-const getFirstBp = require('../util/getFirstBp');
+const getFirstBp = require('./util/getFirstBp');
 
 module.exports = function({ addComponents, theme }) {
   const widths = theme('mainColWidths', {});
   const outerGutters = theme('outerGutters', {});
   const className = '.container';
   const firstBp = getFirstBp(theme);
+  let prevGutter = -1;
+  let prevWidth = -1;
 
   const containerStyles = _.map(widths, (width, bp) => {
     let styles = {};
+    let gutter = outerGutters[bp];
 
-    if (widths[bp] === 'fluid') {
+    if (prevWidth === width && prevGutter === gutter) {
+      return;
+    }
+
+    prevGutter = gutter;
+    prevWidth = width;
+
+    if (width === 'auto' || width === 'fluid') {
       styles = {
-        width: `calc(100vw - (${outerGutters[bp]} * 2))`
+        width: `calc(100% - ${parseInt(gutter, 10) * 2}px)`
       };
     } else {
       styles = {
-        width: widths[bp],
-        'padding-right': 0,
-        'padding-left': 0
+        width: width
       };
     }
 
     if (bp === firstBp) {
       return {
         [className]: {
+          ...styles,
           'margin-right': 'auto',
-          'margin-left': 'auto',
-          ...styles
+          'margin-left': 'auto'
         }
       };
     } else {
@@ -43,9 +51,7 @@ module.exports = function({ addComponents, theme }) {
 
   const nestedContainer = {
     [`${className} ${className}`]: {
-      width: 'auto',
-      'padding-right': 0,
-      'padding-left': 0
+      width: 'auto'
     }
   };
 
