a5bf0fa0f68b3a733a6946c3e10b5e119d8b3ec4
Mike Byrne <mike@area17.com>
2020-11-23 19:51:50 +0000

    refactor Spacing

--- src/Spacing/index.js
+++ src/Spacing/index.js
@@ -1,38 +1,50 @@
-const _ = require('lodash');
 const getFirstBp = require('../util/getFirstBp');
 
-module.exports = function({ addComponents, theme }) {
+module.exports = function({ addBase, theme }) {
   const spacingGroups = theme('spacingGroups', {});
-  const prefixes = theme('spacingGroupProperties', {
-    mt: 'margin-top',
-    pt: 'padding-top'
-  });
+  const cssSpacingProps = {
+    m: ['margin'],
+    mt: ['margin-top'],
+    mb: ['margin-bottom'],
+    mr: ['margin-right'],
+    ml: ['margin-left'],
+    mx: ['margin-left', 'margin-right'],
+    my: ['margin-top', 'margin-bottom'],
+    p: ['padding'],
+    pt: ['padding-top'],
+    pb: ['padding-bottom'],
+    pr: ['padding-right'],
+    pl: ['padding-left'],
+    px: ['padding-left', 'padding-right'],
+    py: ['padding-top', 'padding-bottom']
+  }
   const firstBp = getFirstBp(theme);
-  let spacingStyles = [];
+  const rootStyles = {
+    ':root': {}
+  };
+  const spacingStyles = {};
 
-  _.forEach(prefixes, (property, prefix) => {
-    _.forEach(spacingGroups, (group, name) => {
-      const className = `.${prefix}-${name}`;
-
-      _.forEach(group, (value, bp) => {
-        if (bp === firstBp) {
-          spacingStyles.push({
-            [className]: {
-              [property]: value
-            }
+  Object.entries(spacingGroups).forEach(group => {
+    const [name, spacings] = group;
+    Object.entries(spacings).forEach(spacing => {
+      const [bp, space] = spacing;
+      if (bp === firstBp) {
+        rootStyles[':root'][`--spacing-${ name }`] = space;
+        // create utility class
+        Object.entries(cssSpacingProps).forEach(spacingProp => {
+          const [prefix, cssProps] = spacingProp;
+          spacingStyles[`.${prefix}-${name}`] = spacingStyles[`.${prefix}-${name}`] || {};
+          cssProps.forEach(prop => {
+            spacingStyles[`.${prefix}-${name}`][prop] = `var(--spacing-${name})`;
           });
-        } else {
-          spacingStyles.push({
-            [`@screen ${bp}`]: {
-              [className]: {
-                [property]: value
-              }
-            }
-          });
-        }
-      });
+        });
+      } else {
+        rootStyles[`@screen ${bp}`] = rootStyles[`@screen ${bp}`] || { ':root': {} };
+        rootStyles[`@screen ${bp}`][':root'][`--spacing-${ name }`] = space;
+      }
     });
   });
 
-  addComponents(spacingStyles);
+  addBase(rootStyles);
+  addBase(spacingStyles);
 };
