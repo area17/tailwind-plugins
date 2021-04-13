f3e597a66659c416f017645e20ed72618ca5de4c
Tim Brook <tim@area17.com>
2021-04-13 13:11:06 -0500

    Update ApplyColorVariables to support objects used for color declaration in tailwind

--- src/ApplyColorVariables/index.js
+++ src/ApplyColorVariables/index.js
@@ -1,10 +1,18 @@
 module.exports = function(tokens, colors) {
   Object.entries(colors).forEach((item) => {
     const [name, color] = item;
-    const found = Object.keys(tokens).find((key) => key === color);
-    if (found) {
-      colors[name] = `var(--${found})`;
-    }
+    const colorSplit = color.split('-');
+    const found = Object.entries(tokens).find((token) => {
+      const [key, value] = token;
+      return key === color || key === colorSplit[0];
+    });
+
+    if (!found) return;
+
+    const colorVal =
+      typeof found[1] === 'string' ? found[1] : found[1][colorSplit[1]];
+
+    colors[name] = `var(--${colorVal})`;
   });
 
   return colors;
