d264e44e7b1bcd501e440f7abcfa39411e74f874
Tim Brook <tim@area17.com>
2021-04-13 11:34:37 -0500

    update colour to color

--- src/ApplyColorVariables/index.js
+++ src/ApplyColorVariables/index.js
@@ -1,13 +1,11 @@
 module.exports = function(tokens, colors) {
-
-  Object.entries(colors).forEach(item => {
-    const [name, colour] = item;
-    const found = Object.keys(tokens).find(key => key === colour);
+  Object.entries(colors).forEach((item) => {
+    const [name, color] = item;
+    const found = Object.keys(tokens).find((key) => key === color);
     if (found) {
-      colors[name] = `var(--${ found })`;
+      colors[name] = `var(--${found})`;
     }
   });
 
   return colors;
-
 };
