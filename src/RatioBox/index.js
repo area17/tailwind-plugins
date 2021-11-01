1f12b4b637c3c4ad5ca458fcafacdc5dc8b70ba1
Mike Byrne <mike@area17.com>
2021-11-01 16:49:33 +0000

    added refactored RatioBox with new expandable box and ratio freer classes

--- src/RatioBox/index.js
+++ src/RatioBox/index.js
@@ -1,114 +1,88 @@
-module.exports = function({ addComponents, theme }) {
-  const ratios = theme('ratios', {});
-  const prefix = 'ratio';
-  const bps = Object.keys(theme('screens', {})) || [];
-  const firstBp = bps[0];
-  const bpsWithoutFirstBp = bps.filter((bp) => bp !== firstBp);
-  const ratioBoxStyles = [];
-  const addRatio = (key, ratio) => {
-    ratioBoxStyles.push({
-      [`.${prefix}-${key}::before`]: {
-        'padding-bottom': `${ratio}%`
-      }
-    });
-  };
-  const addRatioWithBp = (key, ratio, bp, displayBp = true) => {
-    const bpPrefix = displayBp ? `.${bp}\\:` : '.';
-    ratioBoxStyles.push({
-      [`@screen ${bp}`]: {
-        [`${bpPrefix}${prefix}-${key}::before`]: {
-          'padding-bottom': `${ratio}%`
-        }
-      }
-    });
-  };
-  const getRatioPercent = (ratio) => Math.round((1 / ratio  + Number.EPSILON) * 100000) / 1000;
-  const isValidRatioFromString = (stringRatio) =>
-    stringRatio
-      .map((value) => Number(value))
-      .every((value) => typeof value === 'number');
+module.exports = function({ addUtilities, theme, config }) {
 
-  if (Object.keys(ratios).length > 0) {
-    ratioBoxStyles.push([
-      {
-        [`.${prefix}`]: {
-          position: 'relative',
-          overflow: 'hidden'
-        },
-        [`.${prefix} > *:first-child, .${prefix}-content`]: {
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          top: 0,
-          bottom: 0
-        },
-        [`.${prefix}::before`]: {
-          content: 'attr(👻)',
-          display: 'block',
-          height: 0,
-          'padding-bottom': '56.25%'
-        }
+  const ratios = theme('ratios', {});
+  const prefixString = config('prefix');
+
+  let styles = {
+    '.ratio': {
+      '--ratio': '100%',
+      'display': 'block',
+      'position': 'relative',
+      'overflow': 'hidden',
+    },
+    '.ratio::before': {
+      'content': 'attr(👻)',
+      'display': 'block',
+      'width': '100%',
+      'height': '0',
+      'padding-bottom': 'var(--ratio)',
+    },
+    [`.ratio > [class*="${prefixString}ratio-content"]`]: {
+      'position': 'absolute',
+      'left': '0',
+      'right': '0',
+      'top': '0',
+      'bottom': '0',
+      'width': '100%',
+      'height': '100%',
+    },
+    '.ratio-expandable::before': {
+      'float': 'left',
+      'width': '1px',
+      'margin-left': '-1px',
+    },
+    '.ratio-expandable::after': {
+      'content': 'attr(👻)',
+      'display': 'table',
+      'clear': 'both',
+    },
+    '.ratio-free::before': {
+      'content': 'unset',
+    },
+    '.ratio-free::after': {
+      'content': 'unset',
+    },
+    [`.ratio-free > [class*="${prefixString}ratio-content"]`]: {
+      'position': 'static',
+      'left': 'auto',
+      'right': 'auto',
+      'top': 'auto',
+      'bottom': 'auto',
+      'width': 'auto',
+      'height': 'auto',
+    },
+    [`.ratio-free > [class*="${prefixString}ratio-content"][class*="${prefixString}w-full"]`]: {
+      'width': '100%',
+    },
+    [`.ratio-free > [class*="${prefixString}ratio-content"][class*="${prefixString}h-auto"]`]: {
+      'height': 'auto',
+    }
+  };
+
+  for (const [key, ratio] of Object.entries(ratios)) {
+      let ratioFloat = 1;
+      let ratioPercent = 100;
+      let r = ratio;
+
+      if (typeof ratio === 'string') {
+        r = r.replace(/[:\/]/, 'x');
+        let ratioParts = r.split('x');
+        ratioFloat = ratioParts[1] / ratioParts[0];
       }
-    ]);
+
+      if (typeof ratio === 'number') {
+        ratioFloat = ratio;
+      }
+
+      ratioPercent = ratioFloat * 100;
+      ratioPercent = Math.round((ratioPercent + Number.EPSILON) * 100) / 100;
+
+      styles[`.ratio-${key}`] = {
+        '--ratio': `${ratioPercent}%`
+      }
   }
 
-  const ratiosTokens = Object.keys(ratios).filter((key) => key !== 'sets');
-  const ratiosSets = ratios && ratios.sets ? Object.keys(ratios.sets) : [];
-
-  ratiosTokens.forEach((key) => {
-    const ratio = ratios[key];
-    switch (typeof ratio) {
-      case 'string':
-        const splitRatio = ratio.split(':');
-        if (isValidRatioFromString(splitRatio)) {
-          addRatio(key, getRatioPercent(splitRatio[0] / splitRatio[1]));
-          bpsWithoutFirstBp.forEach((bp) => {
-            addRatioWithBp(
-              key,
-              getRatioPercent(splitRatio[0] / splitRatio[1]),
-              bp
-            );
-          });
-        }
-        break;
-      case 'number':
-        addRatio(key, getRatioPercent(ratio));
-        bpsWithoutFirstBp.forEach((bp) => {
-          addRatioWithBp(key, getRatioPercent(ratio), bp);
-        });
-        break;
-    }
+  addUtilities(styles, {
+    variants:['responsive']
   });
-
-  ratiosSets.forEach((key) => {
-    const ratio = ratios.sets[key];
-    Object.keys(ratio).forEach((bp) => {
-      if (bp === firstBp) {
-        if (typeof ratio[bp] === 'number') {
-          addRatio(key, getRatioPercent(ratio[bp]));
-        } else if (typeof ratio[bp] === 'string') {
-          const splitRatio = ratio[bp].split(':');
-          if (isValidRatioFromString(splitRatio)) {
-            addRatio(key, getRatioPercent(splitRatio[0] / splitRatio[1]));
-          }
-        }
-      } else {
-        if (typeof ratio[bp] === 'number') {
-          addRatioWithBp(key, getRatioPercent(ratio[bp]), bp, false);
-        } else if (typeof ratio[bp] === 'string') {
-          const splitRatio = ratio[bp].split(':');
-          if (isValidRatioFromString(splitRatio)) {
-            addRatioWithBp(
-              key,
-              getRatioPercent(splitRatio[0] / splitRatio[1]),
-              bp,
-              false
-            );
-          }
-        }
-      }
-    });
-  });
-
-  addComponents(ratioBoxStyles);
 };
