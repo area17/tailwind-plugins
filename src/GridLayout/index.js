c700c4576b790eee43c5c01eb8c705f73ccf4784
Mike Byrne <mike@area17.com>
2021-12-13 19:00:04 +0000

    adds `GridLayout` plugin
    Shout out to @kylegoines

---
+++ src/GridLayout/index.js
@@ -0,0 +1,39 @@
+module.exports = function({ addComponents, theme }) {
+  const breakpoints = theme('screens');
+
+  const styles = [
+    {
+      '.grid-layout': {
+        display: 'grid',
+        'grid-template-columns': 'repeat(var(--grid-columns), 1fr)',
+        'grid-gap': 'var(--inner-gutter)'
+      }
+    }
+  ];
+
+  const columnCount = theme('columnCount', {});
+  const maxCols = theme('maxGridCols', columnCount);
+  const maxColAmount = Math.max.apply(Math, Object.values(maxCols));
+
+  for (let n = 1; n <= maxColAmount + 1; n++) {
+    styles.push({
+      [`.grid-col-span-${n}`]: {
+        '--grid-columns': `${n}`,
+        'grid-column': `span ${n} / span ${n}`
+      }
+    });
+  }
+
+  for (let n = 1; n <= maxColAmount + 1; n++) {
+    styles.push({
+      [`.grid-col-start-${n}`]: {
+        'grid-column-start': `${n}`
+      },
+      [`.grid-col-end-${n}`]: {
+        'grid-column-end': `${n}`
+      }
+    });
+  }
+
+  addComponents(styles);
+};
