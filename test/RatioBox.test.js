358184943162075c82bd53619bdb15227beff181
Joe Critchley <joecritch@gmail.com>
2022-06-22 18:23:28 +0100

    updated jest. updated tests to use snapshots instead of matchCSS. updated tests to support JIT.

--- test/RatioBox.test.js
+++ test/RatioBox.test.js
@@ -1,10 +1,5 @@
 const generatePluginCss = require('./generatePluginCss');
-const cssMatcher = require('jest-matcher-css');
 const { RatioBox } = require('../index');
-
-expect.extend({
-  toMatchCss: cssMatcher
-});
 
 describe('spacing plugin', () => {
   it('exists', () => {
@@ -14,47 +9,22 @@ describe('spacing plugin', () => {
   test('generates correct css', () => {
     return generatePluginCss(RatioBox, {
       theme: {
-        ratios: [
-          '1:1',
-          '16:9'
-        ]
+        screens: {
+          lg: '1024px'
+        },
+        ratios: {
+          '1x1': '1:1',
+          '16x9': '16:9'
+        }
       }
     }).then((css) => {
-      expect(css).toMatchCss(`
-      .ratio {
-        position: relative;
-        overflow: hidden;
-      }
-
-      .ratio::before {
-        content: 'attr(👻)';
-        display: block;
-        height: 0;
-        padding-bottom: 56.25%;
-      }
-
-      .ratio-content {
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-      }
-
-      .ratio-1\\:1::before {
-        padding-bottom: 100%;
-      }
-
-      .ratio-16\\:9::before {
-        padding-bottom: 56.25%;
-      }
-      `);
+      expect(css).toMatchSnapshot();
     });
   });
 
   test("doesn't output anything when no options set", () => {
     return generatePluginCss(RatioBox, {}).then((css) => {
-      expect(css).toMatchCss(``);
+      expect(css).toMatch('');
     });
   });
 });
