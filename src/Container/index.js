193daa7debaf08de663b040f7702f36d93e127a8
Antoine Doury <antoine@area17.com>
2024-02-13 15:51:42 +0100

    Container, FullBleed, BackgroundFill : support global class prefix

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,54 +1,72 @@
-module.exports = function ({ addBase }) {
-  let styles = {
-    '.container': {
-      width:
-        'calc(var(--container-width, 100%) - (2 * var(--breakout-container-outer-gutter, var(--container-outer-gutter, var(--outer-gutter, 0)))))',
-      'margin-right': 'auto',
-      'margin-left': 'auto',
-    },
-    '.container > *': {
-      '--container-outer-gutter': '0',
-      '--breakout-container-outer-gutter': '0',
-    },
-    '.container-reset': {
+module.exports = function ({ addBase, prefix }) {
+  // handle global prefix
+  const classNameContainer = prefix('.container');
+  const classNameBreakout = prefix('.breakout');
+
+  let styles = {};
+  styles[classNameContainer] = {
+    width:
+      'calc(var(--container-width, 100%) - (2 * var(--breakout-container-outer-gutter, var(--container-outer-gutter, var(--outer-gutter, 0)))))',
+    'margin-right': 'auto',
+    'margin-left': 'auto',
+  };
+
+  styles[`${classNameContainer}  > *`] = {
+    '--container-outer-gutter': '0',
+    '--breakout-container-outer-gutter': '0',
+  };
+
+  styles[`${classNameContainer}-reset`] = {
       width: 'unset',
       'margin-right': 'unset',
       'margin-left': 'unset',
-    },
-    '.container-reset > *': {
-      '--container-outer-gutter': 'var(--outer-gutter, 0)',
-      '--breakout-container-outer-gutter': 'inherit',
-    },
-    '.breakout': {
-      '--breakout-outer-gutter':
-        'max(var(--outer-gutter), calc((100% - var(--container-width, 100%)) / 2))',
-      '--breakout-container-outer-gutter': 'var(--outer-gutter)',
-      position: 'relative',
-      left: '50%',
-      width: 'calc(100vw - var(--scrollbar-visible-width, 0px))',
-      'margin-left': 'calc((100vw - var(--scrollbar-visible-width, 0px)) / -2)',
-    },
-    ['.breakout.px-outer-gutter, .breakout > .px-outer-gutter']: {
-      'padding-left': 'var(--breakout-outer-gutter)',
-      'padding-right': 'var(--breakout-outer-gutter)',
-    },
-    ['.breakout.pr-outer-gutter, .breakout > .pr-outer-gutter']: {
-      'padding-right': 'var(--breakout-outer-gutter)',
-    },
-    ['.breakout.pl-outer-gutter, .breakout > .pl-outer-gutter']: {
-      'padding-left': 'var(--breakout-outer-gutter)',
-    },
-    '.breakout > .w-outer-gutter': {
-      width: 'var(--breakout-outer-gutter)',
-    },
-    '.breakout-reset': {
-      '--breakout-outer-gutter': 'var(--outer-gutter)',
-      '--breakout-container-outer-gutter': '0',
-      position: 'unset',
-      left: 'unset',
-      width: 'unset',
-      'margin-left': 'unset',
-    },
+  };
+
+  styles[`${classNameContainer}-reset > *`] = {
+    '--container-outer-gutter': 'var(--outer-gutter, 0)',
+    '--breakout-container-outer-gutter': 'inherit',
+  };
+
+  styles[classNameBreakout] = {
+    '--breakout-outer-gutter':
+      'max(var(--outer-gutter), calc((100% - var(--container-width, 100%)) / 2))',
+    '--breakout-container-outer-gutter': 'var(--outer-gutter)',
+    position: 'relative',
+    left: '50%',
+    width: 'calc(100vw - var(--scrollbar-visible-width, 0px))',
+    'margin-left': 'calc((100vw - var(--scrollbar-visible-width, 0px)) / -2)',
+  };
+
+  styles[
+    `${classNameBreakout}.px-outer-gutter, ${classNameBreakout} > .px-outer-gutter`
+  ] = {
+    'padding-left': 'var(--breakout-outer-gutter)',
+    'padding-right': 'var(--breakout-outer-gutter)',
+  };
+
+  styles[
+    `${classNameBreakout}.pr-outer-gutter, ${classNameBreakout} > .pr-outer-gutter`
+  ] = {
+    'padding-right': 'var(--breakout-outer-gutter)',
+  };
+
+  styles[
+    `${classNameBreakout}.pl-outer-gutter, ${classNameBreakout} > .pl-outer-gutter`
+  ] = {
+    'padding-left': 'var(--breakout-outer-gutter)',
+  };
+
+  styles[`${classNameBreakout} > .w-outer-gutter`] = {
+    width: 'var(--breakout-outer-gutter)',
+  };
+
+  styles[`${classNameBreakout}-reset`] = {
+    '--breakout-outer-gutter': 'var(--outer-gutter)',
+    '--breakout-container-outer-gutter': '0',
+    position: 'unset',
+    left: 'unset',
+    width: 'unset',
+    'margin-left': 'unset',
   };
 
   addBase(styles);
--- src/FullBleedScroller/index.js
+++ src/FullBleedScroller/index.js
@@ -1,40 +1,41 @@
-module.exports = function ({ addBase }) {
-  let styles = {
-    '.full-bleed-scroller': {
-      display: 'flex',
-      'flex-flow': 'row nowrap',
-      'overflow-x': 'auto',
-      'overflow-y': 'hidden',
-      gap: 'var(--inner-gutter)',
-    },
-    '.full-bleed-scroller::before': {
-      content: "''",
-      flex: '0 0 auto',
-      width:
-        'calc(var(--breakout-outer-gutter, var(--outer-gutter, 0px)) - var(--inner-gutter, 0px))',
-    },
-    '.full-bleed-scroller::after': {
-      content: "''",
-      flex: '0 0 auto',
-      width:
-        'calc(var(--breakout-outer-gutter, var(--outer-gutter, 0px)) - var(--inner-gutter, 0px))',
-    },
-    '.full-bleed-scroller-reset': {
-      display: 'unset',
-      'flex-flow': 'unset',
-      'flex-wrap': 'unset',
-      'overflow-x': 'unset',
-    },
-    '.full-bleed-scroller-reset::before': {
-      content: 'none',
-      flex: 'unset',
-      width: 'unset',
-    },
-    '.full-bleed-scroller-reset::after': {
-      content: 'none',
-      flex: 'unset',
-      width: 'unset',
-    },
+module.exports = function ({ addBase, prefix }) {
+  const classNameContainer = prefix('.full-bleed-scroller');
+
+  let styles = {};
+  styles[classNameContainer] = {
+    display: 'flex',
+    'flex-flow': 'row nowrap',
+    'overflow-x': 'auto',
+    'overflow-y': 'hidden',
+    gap: 'var(--inner-gutter)',
+  };
+  styles[`${classNameContainer}::before`] = {
+    content: "''",
+    flex: '0 0 auto',
+    width:
+      'calc(var(--breakout-outer-gutter, var(--outer-gutter, 0px)) - var(--inner-gutter, 0px))',
+  };
+  styles[`${classNameContainer}::after`] = {
+    content: "''",
+    flex: '0 0 auto',
+    width:
+      'calc(var(--breakout-outer-gutter, var(--outer-gutter, 0px)) - var(--inner-gutter, 0px))',
+  };
+  styles[`${classNameContainer}-reset`] = {
+    display: 'unset',
+    'flex-flow': 'unset',
+    'flex-wrap': 'unset',
+    'overflow-x': 'unset',
+  };
+  styles[`${classNameContainer}-reset::before`] = {
+    content: 'none',
+    flex: 'unset',
+    width: 'unset',
+  };
+  styles[`${classNameContainer}-reset::after`] = {
+    content: 'none',
+    flex: 'unset',
+    width: 'unset',
   };
 
   addBase(styles);
