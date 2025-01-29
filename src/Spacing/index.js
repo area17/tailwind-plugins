59922a4a882ca8097a3edbf105531cd15ee3a49c
Mike Byrne <mike@area17.com>
2025-01-29 17:33:05 +0000

    tailwind v4 migration - reinstate responsive spacing class utilities

--- src/Spacing/index.js
+++ src/Spacing/index.js
@@ -1,7 +1,39 @@
-module.exports = function ({ addBase, theme }) {
+module.exports = function ({ addBase, matchUtilities, theme }) {
   const spacingGroups = theme('spacingGroups', {});
   const breakpoints = theme('screens');
   const firstBp = Object.keys(breakpoints)[0];
+  const cssSpacingProps = {
+    m: ['margin'],
+    mt: ['margin-top'],
+    mb: ['margin-bottom'],
+    mr: ['margin-right'],
+    ml: ['margin-left'],
+    mx: ['margin-inline'],
+    my: ['margin-block'],
+    ms: ['margin-inline-start'],
+    me: ['margin-inline-end'],
+    p: ['padding'],
+    pt: ['padding-top'],
+    pb: ['padding-bottom'],
+    pr: ['padding-right'],
+    pl: ['padding-left'],
+    ps: ['padding-inline-start'],
+    pe: ['padding-inline-end'],
+    px: ['padding-inline'],
+    py: ['padding-block'],
+    gap: ['gap'],
+    'gap-x': ['column-gap'],
+    'gap-y': ['row-gap'],
+    top: ['top'],
+    bottom: ['bottom'],
+    left: ['left'],
+    right: ['right'],
+    start: ['inset-inline-start'],
+    end: ['inset-inline-end'],
+    inset: ['inset'],
+    'inset-x': ['inset-inline'],
+    'inset-y': ['inset-block'],
+  };
   const rootStyles = {
     ':root': {},
   };
@@ -39,6 +71,65 @@ module.exports = function ({ addBase, theme }) {
     }
   });
 
-  // add
+  // add root styles
   addBase(rootStyles);
+
+  // add utility classes
+  const valuesCalc = () => {
+    let obj = {};
+    Object.keys(spacingGroups).forEach((group) => {
+      obj[group] = group;
+    });
+    return obj;
+  };
+  const values = valuesCalc();
+  const spacingStyles = {};
+
+  function add(cssSelector, cssAttributes) {
+    let selector = {};
+    selector[`${cssSelector}`] = (value) => {
+      if (value.indexOf('var(') > -1) {
+        return {};
+      }
+      let negative = typeof(value) === 'string' && value.indexOf('calc') > -1;
+      let attributes = {};
+
+      if (negative) {
+        value = value.replace(/calc\(/i, '');
+        value = value.replace(/ \* -1\)/i, '');
+
+        if (typeof(cssAttributes) === 'string') {
+          attributes[cssAttributes] = `calc(var(--spacing-${value}) * -1)`;
+        }
+
+        if (typeof(cssAttributes) === 'object' && Array.isArray(cssAttributes)) {
+          cssAttributes.forEach(cssAttribute => {
+            attributes[cssAttribute] = `calc(var(--spacing-${value}) * -1)`;
+          });
+        }
+      } else {
+        if (typeof(cssAttributes) === 'string') {
+          attributes[cssAttributes] = `var(--spacing-${value})`;
+        }
+
+        if (typeof(cssAttributes) === 'object' && Array.isArray(cssAttributes)) {
+          cssAttributes.forEach(cssAttribute => {
+            attributes[cssAttribute] = `var(--spacing-${value})`;
+          });
+        }
+      }
+
+      return attributes;
+    };
+
+    matchUtilities(selector, {
+      values: values,
+      supportsNegativeValues: true,
+    });
+  }
+
+  Object.keys(cssSpacingProps).forEach(prop => {
+    let attributes = cssSpacingProps[prop];
+    add(prop, attributes);
+  });
 };
