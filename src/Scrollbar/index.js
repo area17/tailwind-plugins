07f6ff495c7a1c1779f0c83fb9a0d88b8444f5d0
Mike Byrne <mike@area17.com>
2025-02-04 18:53:54 +0000

    refactor scrollbars to only use components and utilities

--- src/Scrollbar/index.js
+++ src/Scrollbar/index.js
@@ -1,4 +1,4 @@
-module.exports = function ({ addBase, addComponents, matchComponents, theme, prefix, config }) {
+module.exports = function ({ matchComponents, addUtilities, matchUtilities, theme, prefix, config }) {
   const classNameScrollbar = prefix('.scrollbar');
   const prefixString = config('prefix');
   const trackColors = {
@@ -19,7 +19,7 @@ module.exports = function ({ addBase, addComponents, matchComponents, theme, pre
       if (type === 'bg') {
         trackColorsValues[`${type}-${name}`] = setting;
       } else if (type === 'token') {
-        trackColorsValues[name] = `var(--${name})`;
+        trackColorsValues[name] = `var(--color-${name})`;
       } else {
         trackColorsValues[name] = setting;
       }
@@ -32,54 +32,71 @@ module.exports = function ({ addBase, addComponents, matchComponents, theme, pre
       if (type === 'bg') {
         thumbColorsValues[`${type}-${name}`] = setting;
       } else if (type === 'token') {
-        thumbColorsValues[name] = `var(--${name})`;
+        thumbColorsValues[name] = `var(--color-${name})`;
       } else {
         thumbColorsValues[name] = setting;
       }
     }
   });
 
-  // base styles (always added to the CSS regardless of usage in site)
-  const baseStyles = {
-    [`[class*='${prefixString}scrollbar-track-'], [class*='${prefixString}scrollbar-thumb-']`]:
-      {
-        '--scrollbar-bg': '#fafafa',
-        '--scrollbar-fg': '#c1c1c1',
-        '--scrollbar-border': '#e8e8e8',
-        'scrollbar-color': 'var(--scrollbar-fg) var(--scrollbar-bg)',
-      },
-    [`[class*='${prefixString}scrollbar-track-']::-webkit-scrollbar, [class*='${prefixString}scrollbar-thumb-']::-webkit-scrollbar`]:
-      {
-        width: 'var(--scrollbar-width, 15px)',
-        height: 'var(--scrollbar-width, 15px)',
-      },
-    [`[class*='${prefixString}scrollbar-track-']::-webkit-scrollbar-track, [class*='${prefixString}scrollbar-thumb-']::-webkit-scrollbar-track`]:
-      {
-        background: 'var(--scrollbar-bg)',
-      },
-    [`[class*='${prefixString}scrollbar-track-']::-webkit-scrollbar-track:horizontal, [class*='${prefixString}scrollbar-thumb-']::-webkit-scrollbar-track:horizontal`]:
-      {
-        'border-block-start': '1px solid var(--scrollbar-border)',
-        'border-block-end': '1px solid var(--scrollbar-border)',
-      },
-    [`[class*='${prefixString}scrollbar-track-']::-webkit-scrollbar-track:vertical, [class*='${prefixString}scrollbar-thumb-']::-webkit-scrollbar-track:vertical`]:
-      {
-        'border-inline-start': '1px solid var(--scrollbar-border)',
-        'border-inline-end': '1px solid var(--scrollbar-border)',
-      },
-    [`[class*='${prefixString}scrollbar-track-']::-webkit-scrollbar-thumb, [class*='${prefixString}scrollbar-thumb-']::-webkit-scrollbar-thumb`]:
-      {
-        background: 'var(--scrollbar-fg)',
-        'border-radius': '20px',
-        border: 'var(--scrollbar-padding, 4px) solid transparent',
-        'background-clip': 'content-box',
-      },
+  const componentValues = {
+    thin: 'thin',
+    'thin-collapse': 'thin-collapse',
   };
+  ['track', 'thumb'].forEach(scrollBarNode => {
+    Object.keys(thumbColors).forEach(type => {
+      for (const [name, setting] of Object.entries(thumbColors[type])) {
+        if (type === 'bg') {
+          componentValues[`${scrollBarNode}-${type}-${name}`] = `${scrollBarNode}-${type}-${name}`;
+        } else if (type === 'token') {
+          componentValues[`${scrollBarNode}-${name}`] = `${scrollBarNode}-${type}-${name}`;
+        } else {
+          componentValues[`${scrollBarNode}-${name}`] = `${scrollBarNode}-${type}-${name}`;
+        }
+      }
+    });
+  });
 
-  addBase(baseStyles);
+  // add base styles when required
+  matchComponents(
+    {
+      [`${prefixString}scrollbar`]: (value) => {
+        return {
+          '--scrollbar-bg': '#fafafa',
+          '--scrollbar-fg': '#c1c1c1',
+          '--scrollbar-border': '#e8e8e8',
+          'scrollbar-color': 'var(--scrollbar-fg) var(--scrollbar-bg)',
+          '&::-webkit-scrollbar': {
+            width: 'var(--scrollbar-width, unset)',
+            height: 'var(--scrollbar-width, unset)',
+          },
+          '&::-webkit-scrollbar-track': {
+            background: 'var(--scrollbar-bg)',
+          },
+          '&::-webkit-scrollbar-track:horizontal': {
+            'border-block-start': '1px solid var(--scrollbar-border)',
+            'border-block-end': '1px solid var(--scrollbar-border)',
+          },
+          '&::-webkit-scrollbar-track:vertical': {
+            'border-inline-start': '1px solid var(--scrollbar-border)',
+            'border-inline-end': '1px solid var(--scrollbar-border)',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            background: 'var(--scrollbar-fg)',
+            'border-radius': '20px',
+            border: 'var(--scrollbar-padding, 4px) solid transparent',
+            'background-clip': 'content-box',
+          },
+        };
+      },
+    },
+    {
+      values: componentValues,
+    }
+  );
 
-  // add component styles (only added to CSS when required)
-  const componentStyles = {
+  // add utility styles (only added to CSS when required)
+  addUtilities({
     [`${classNameScrollbar}-none`]: {
       '-ms-overflow-style': 'none',
       'scrollbar-width': 'none',
@@ -90,9 +107,6 @@ module.exports = function ({ addBase, addComponents, matchComponents, theme, pre
     [`${classNameScrollbar}-thin, ${classNameScrollbar}-thin-collapse`]: {
       '--scrollbar-width': '10px',
       '--scrollbar-padding': '2px',
-      '--scrollbar-bg': '#fafafa',
-      '--scrollbar-fg': '#c1c1c1',
-      '--scrollbar-border': '#e8e8e8',
       'scrollbar-width': 'thin',
     },
     [`${classNameScrollbar}-thin-collapse`]: {
@@ -100,61 +114,33 @@ module.exports = function ({ addBase, addComponents, matchComponents, theme, pre
       '--scrollbar-padding': '0px',
       'scrollbar-width': 'thin',
     },
-    [`${classNameScrollbar}-thin::-webkit-scrollbar, ${classNameScrollbar}-thin-collapse::-webkit-scrollbar`]:
-      {
-        width: 'var(--scrollbar-width, unset)',
-        height: 'var(--scrollbar-width, unset)',
-      },
-    [`${classNameScrollbar}-thin::-webkit-scrollbar-track, ${classNameScrollbar}-thin-collapse::-webkit-scrollbar-track`]:
-      {
-        background: 'var(--scrollbar-bg)',
-      },
-    [`${classNameScrollbar}-thin::-webkit-scrollbar-track:horizontal, ${classNameScrollbar}-thin-collapse::-webkit-scrollbar-track:horizontal`]:
-      {
-        'border-top': '1px solid var(--scrollbar-border)',
-        'border-bottom': '1px solid var(--scrollbar-border)',
-      },
-    [`${classNameScrollbar}-thin::-webkit-scrollbar-track:vertical, ${classNameScrollbar}-thin-collapse::-webkit-scrollbar-track:vertical`]:
-      {
-        'border-inline-start': '1px solid var(--scrollbar-border)',
-        'border-inline-end': '1px solid var(--scrollbar-border)',
-      },
-    [`${classNameScrollbar}-thin::-webkit-scrollbar-thumb, ${classNameScrollbar}-thin-collapse::-webkit-scrollbar-thumb`]:
-      {
-        background: 'var(--scrollbar-fg)',
-        'border-radius': '20px',
-        border: 'var(--scrollbar-padding) solid transparent',
-        'background-clip': 'content-box',
-      },
-  };
-
-  addComponents(componentStyles);
+  });
 
   // add colour styles (only added to CSS when required)
-  matchComponents(
+  matchUtilities(
     {
       [`${prefixString}scrollbar-track`]: (value) => {
         return {
           '--scrollbar-bg': value,
           '--scrollbar-border': 'var(--scrollbar-bg)',
-        }
+        };
       },
     },
     {
       values: trackColorsValues,
-    },
+    }
   );
 
-  matchComponents(
+  matchUtilities(
     {
       [`${prefixString}scrollbar-thumb`]: (value) => {
         return {
           '--scrollbar-fg': value,
-        }
+        };
       },
     },
     {
       values: thumbColorsValues,
-    },
+    }
   );
 };
