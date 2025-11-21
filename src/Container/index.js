63a0717d753549c59f709003f329adcc8c6a70c4
Mike Byrne <mike@area17.com>
2025-11-21 16:33:15 +0000

    v5.1.3 - update `Container` plugin to fix nested selector warnings

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,54 +1,57 @@
 module.exports = function ({ addUtilities, prefix }) {
   // handle global prefix
-  const classNameContainer = prefix('.container[class]');
-  const classNameBreakout = prefix('.breakout[class]');
+  const classNameContainer = prefix('.container');
+  const classNameBreakout = prefix('.breakout');
+  const specificity = '[class]';
 
   const styles = {
-    [classNameContainer]: {
+    [`${classNameContainer}${specificity}`]: {
       width:
         'calc(var(--container-width, 100%) - (2 * var(--breakout-container-outer-gutter, var(--container-outer-gutter, var(--outer-gutter, 0)))))',
       'margin-right': 'auto',
       'margin-left': 'auto',
     },
-    [`${classNameContainer} > *`]: {
+    [`${classNameContainer}${specificity} > *`]: {
       '--container-outer-gutter': '0',
       '--breakout-container-outer-gutter': '0',
     },
-    [`${classNameContainer}-reset`]: {
+    [`${classNameContainer}-reset${specificity}`]: {
       width: 'unset',
       'margin-right': 'unset',
       'margin-left': 'unset',
     },
-    [`${classNameContainer}-reset > *`]: {
+    [`${classNameContainer}-reset${specificity} > *`]: {
       '--container-outer-gutter': 'var(--outer-gutter, 0)',
       '--breakout-container-outer-gutter': 'inherit',
     },
-    [`${classNameBreakout}, ${classNameContainer} > ${classNameBreakout}`]: {
-      '--breakout-outer-gutter':
-        'max(var(--outer-gutter), calc((100% - var(--container-width, 100%)) / 2))',
-      '--breakout-container-outer-gutter': 'var(--outer-gutter)',
-      position: 'relative',
-      'inset-inline-start': '50%',
-      width: 'calc(100vw - var(--scrollbar-visible-width, 0px))',
-      'margin-inline-start': 'calc((100vw - var(--scrollbar-visible-width, 0px)) / -2)',
-    },
-    [`${classNameBreakout}.px-outer-gutter, ${classNameBreakout} > .px-outer-gutter`]:
+    [`${classNameBreakout}${specificity}, ${classNameContainer}${specificity} > ${classNameBreakout}${specificity}`]:
+      {
+        '--breakout-outer-gutter':
+          'max(var(--outer-gutter), calc((100% - var(--container-width, 100%)) / 2))',
+        '--breakout-container-outer-gutter': 'var(--outer-gutter)',
+        position: 'relative',
+        'inset-inline-start': '50%',
+        width: 'calc(100vw - var(--scrollbar-visible-width, 0px))',
+        'margin-inline-start':
+          'calc((100vw - var(--scrollbar-visible-width, 0px)) / -2)',
+      },
+    [`${classNameBreakout}${specificity}.px-outer-gutter, ${classNameBreakout}${specificity} > .px-outer-gutter`]:
       {
         'padding-inline-start': 'var(--breakout-outer-gutter)',
         'padding-inline-end': 'var(--breakout-outer-gutter)',
       },
-    [`${classNameBreakout}.pr-outer-gutter, ${classNameBreakout} > .pr-outer-gutter`]:
+    [`${classNameBreakout}${specificity}.pr-outer-gutter, ${classNameBreakout}${specificity} > .pr-outer-gutter`]:
       {
         'padding-inline-end': 'var(--breakout-outer-gutter)',
       },
-    [`${classNameBreakout}.pl-outer-gutter, ${classNameBreakout} > .pl-outer-gutter`]:
+    [`${classNameBreakout}${specificity}.pl-outer-gutter, ${classNameBreakout}${specificity} > .pl-outer-gutter`]:
       {
         'padding-inline-start': 'var(--breakout-outer-gutter)',
       },
-    [`${classNameBreakout} > .w-outer-gutter`]: {
+    [`${classNameBreakout}${specificity} > .w-outer-gutter`]: {
       width: 'var(--breakout-outer-gutter)',
     },
-    [`${classNameBreakout}-reset`]: {
+    [`${classNameBreakout}-reset${specificity}`]: {
       '--breakout-outer-gutter': 'var(--outer-gutter)',
       '--breakout-container-outer-gutter': '0',
       position: 'unset',
