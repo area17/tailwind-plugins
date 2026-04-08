1031751e5ab2895b61583a43597377d000d4672a
Florrie <florrie@area17.com>
2026-04-08 18:18:47 -0400

    Create ColorThemes plugin and docs

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -20,6 +20,7 @@ const {
   PseudoElements,
   InteractionMediaQueries,
   RatioBox,
+  ColorThemes,
 } = require('../index');
 
 // conf
@@ -48,6 +49,7 @@ module.exports = {
     PseudoElements,
     InteractionMediaQueries,
     RatioBox,
+    ColorThemes,
   ],
   theme: {
     screens: feConfig.structure.breakpoints,
@@ -61,6 +63,7 @@ module.exports = {
     ratios: feConfig.ratios,
     css: feConfig.css,
     colors: feConfig.color.tokens,
+    colorThemes: feConfig.colorThemes,
     borderColor: ApplyColorVariables(
       feConfig.color.tokens,
       feConfig.color.border
